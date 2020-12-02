1c7fc64da912708d54bb04fd39d2727e2b89a36a
John Watson <johnwatson484@hotmail.com>
2020-12-02 14:24:20 +0000

    Part 2 added

--- 2020/day2/part2.js
+++ 2020/day2/part2.js
@@ -4,11 +4,10 @@
   const data = await readFile(path.resolve(__dirname, 'data.txt'))
   const values = data.toString().trim().split('\n').map(x => x.trim().replace(':', '').replace('-', ' ').split(' '))
 
-  const validPasswords = values.filter(x => isValid(x[0], x[1], x[2], x[3]))
+  const validPasswords = values.filter(x => (checkLocation(x[0], x[2], x[3]) !== checkLocation(x[1], x[2], x[3])))
   console.log(validPasswords.length)
 }())
 
-const isValid = (min, max, character, password) => {
-  const occurences = (password.match(new RegExp(character, 'g')) || []).length
-  return occurences >= min && occurences <= max
+const checkLocation = (location, character, password) => {
+  return password[location - 1] === character
 }
