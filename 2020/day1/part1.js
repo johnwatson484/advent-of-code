1db929e75cd28df484203132728d807360227ed8
John Watson <johnwatson484@hotmail.com>
2020-12-02 13:35:27 +0000

    Use path

--- 2020/day1/part1.js
+++ 2020/day1/part1.js
@@ -1,16 +1,17 @@
 (async function () {
   const fs = require('fs')
+  const path = require('path')
   const util = require('util')
   const readFile = util.promisify(fs.readFile)
-  const data = await readFile(__dirname + '/data.txt')
-  const values = data.toString().trim().split("\n").map(x => Number(x))
+  const data = await readFile(path.resolve(__dirname, 'data.txt'))
+  const values = data.toString().trim().split('\n').map(x => Number(x))
 
   const matchedValues = values.filter(x => hasMatch(x, values))
   const result = matchedValues.reduce((x, y) => x * y)
   console.log(result)
 }())
 
-function hasMatch(value, values) {
+function hasMatch (value, values) {
   const matchValue = 2020 - value
   return values.includes(matchValue)
 }
