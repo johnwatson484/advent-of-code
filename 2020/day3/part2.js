c62f1a60b7cff35474d9009670a4a8310b08ba95
John Watson <johnwatson484@hotmail.com>
2020-12-03 17:35:28 +0000

    Part 2 added

--- 2020/day3/part2.js
+++ 2020/day3/part2.js
@@ -3,13 +3,21 @@
   const readFile = require('../../utils/read-file')
   const data = await readFile(path.resolve(__dirname, 'data.txt'))
   const course = data.toString().trim().split('\n').map(x => x.trim().split(''))
-  const treesHit = sled(course)
-  console.log(treesHit)
+  const routes = [
+    [1, 1],
+    [1, 3],
+    [1, 5],
+    [1, 7],
+    [2, 1]
+  ]
+  const treesHit = routes.map(x => sled(course, x[0], x[1]))
+  const result = treesHit.reduce((x, y) => x * y)
+  console.log(result)
 }())
 
 const tree = '#'
 
-const sled = (course) => {
+const sled = (course, down, right) => {
   let treesHit = 0
   const courseDistance = course.length - 1
   const courseWidth = course[0].length - 1
@@ -17,8 +25,8 @@ const sled = (course) => {
   let currentWidth = 0
 
   while (currentDistance < courseDistance) {
-    currentDistance += 1
-    currentWidth += 3
+    currentDistance += down
+    currentWidth += right
     if (currentWidth > courseWidth) {
       currentWidth = currentWidth - courseWidth - 1
     }
