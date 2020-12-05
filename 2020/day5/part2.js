3eeba1cc3115d3afac535911bc67ca3a4aa93db4
John Watson <johnwatson484@hotmail.com>
2020-12-05 16:20:07 +0000

    Refactor

--- 2020/day5/part2.js
+++ 2020/day5/part2.js
@@ -1,9 +1,6 @@
 const path = require('path')
 const readFile = require('../../utils/read-file')
 
-const rows = [...Array(128).keys()]
-const seats = [...Array(8).keys()]
-
 const getHighestSeatId = async () => {
   const data = await readFile(path.resolve(__dirname, 'data.txt'))
   const values = data.toString().trim().split('\n')
@@ -17,20 +14,20 @@ const getHighestSeatId = async () => {
 
 const getSeatId = (pass) => {
   const values = pass.split('')
-  let row = values[0] === 'F' ? rows.slice(0, 64) : rows.slice(64)
-  row = values[1] === 'F' ? row.slice(0, 32) : row.slice(32)
-  row = values[2] === 'F' ? row.slice(0, 16) : row.slice(16)
-  row = values[3] === 'F' ? row.slice(0, 8) : row.slice(8)
-  row = values[4] === 'F' ? row.slice(0, 4) : row.slice(4)
-  row = values[5] === 'F' ? row.slice(0, 2) : row.slice(2)
-  row = (values[6] === 'F' ? row.slice(0, 1) : row.slice(1))[0]
-
-  let seat = values[7] === 'L' ? seats.slice(0, 4) : seats.slice(4)
-  seat = values[8] === 'L' ? seat.slice(0, 2) : seat.slice(2)
-  seat = (values[9] === 'L' ? seat.slice(0, 1) : seat.slice(1))[0]
+  const row = decodePassValue('F', 64, values.slice(0, 7), 128)
+  const seat = decodePassValue('L', 4, values.slice(7), 8)
 
   const validSeat = row !== 0 && row !== 127
   return !validSeat ? -1 : row * 8 + seat
+}
+
+const decodePassValue = (lowerKey, divider, pass, totalItems) => {
+  let items = [...Array(totalItems).keys()]
+  for (let i = 0; i < pass.length; i++) {
+    items = pass[i] === lowerKey ? items.slice(0, divider) : items.slice(divider)
+    divider /= 2
+  }
+  return items[0]
 }
 
 (async function () {
