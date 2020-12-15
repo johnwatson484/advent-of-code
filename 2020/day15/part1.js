c66e617f3eee6c151c588f289e0340d44a8e9852
John Watson <johnwatson484@hotmail.com>
2020-12-15 20:22:50 +0000

    Day 15 added

---
+++ 2020/day15/part1.js
@@ -0,0 +1,24 @@
+const path = require('path')
+const readFile = require('../../utils/read-file')
+
+const getValue = async () => {
+  const data = await readFile(path.resolve(__dirname, 'data.txt'))
+  const values = data.toString().trim().split(',').map(Number)
+
+  for (let i = values.length; i < 2020; i++) {
+    const value = values[i - 1]
+    const occurences = values.filter(x => x === value).length
+    if (occurences === 1) {
+      values.push(0)
+    } else {
+      const previous = values.slice(0, values.length - 1)
+      values.push(i - previous.lastIndexOf(value) - 1)
+    }
+  }
+  console.log(values)
+  console.log(values[values.length - 1])
+}
+
+(async function () {
+  await getValue()
+}())
