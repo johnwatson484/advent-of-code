9f5547661c84e64221218750f945d8cd3bcc97f6
John Watson <johnwatson484@hotmail.com>
2020-12-16 18:12:06 +0000

    Refactored

--- 2020/day15/part2.js
+++ 2020/day15/part2.js
@@ -5,7 +5,7 @@ const getValue = async () => {
   const data = await readFile(path.resolve(__dirname, 'data.txt'))
   const values = data.toString().trim().split(',').map(Number)
 
-  for (let i = values.length; i < 2020; i++) {
+  for (let i = values.length; i < 30000000; i++) {
     const value = values[i - 1]
     const occurences = values.filter(x => x === value).length
     if (occurences === 1) {
