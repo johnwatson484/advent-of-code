56de6541e7237a06d6bc551346f9c3f53d5d9f66
John Watson <johnwatson484@hotmail.com>
2022-12-10 16:32:12 +0000

    Add part 2

--- 2022/Day4/Part2.cs
+++ 2022/Day4/Part2.cs
@@ -3,5 +3,25 @@ public static class Part2
 {
     public static void Run()
     {
+        var lines = File.ReadAllLines(Path.Combine(Directory.GetCurrentDirectory(), "data.txt"));
+        var totalDuplicatePairs = 0;
+        foreach (var pair in lines)
+        {   
+            var elves = pair.Split(',');
+            var elf1Jobs = GetJobs(elves[0]);
+            var elf2Jobs = GetJobs(elves[1]);
+            var overlapJobs = elf1Jobs.Intersect(elf2Jobs).ToList();
+            if(overlapJobs.Count > 0)
+            {
+                totalDuplicatePairs++;
+            }
+        }
+        Console.WriteLine(totalDuplicatePairs);
+    }
+
+    public static IList<int> GetJobs(string elf)
+    {
+        var jobs = elf.Split('-');
+        return Enumerable.Range(int.Parse(jobs[0]), int.Parse(jobs[1]) - int.Parse(jobs[0]) + 1).ToList();
     }
 }
