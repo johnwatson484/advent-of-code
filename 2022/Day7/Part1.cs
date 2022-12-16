4c083aa2037e121ea241c80d292ca28715620221
John Watson <johnwatson484@hotmail.com>
2022-12-16 17:48:39 +0000

    Add day 2

--- 2022/Day7/Part1.cs
+++ 2022/Day7/Part1.cs
@@ -1,29 +1,5 @@
 namespace AdventOfCode;
 
-public class Node
-{
-    public string Name { get; set; }
-    public Node? Parent { get; set; }
-    public List<Node> Children { get; set; } = new();
-    public int Size { get; set; } = 0;
-
-    public Node(string name, Node? parent = null)
-    {
-        Name = name;
-        Parent = parent;
-    }
-
-    public void AddChild(Node child)
-    {
-        Children.Add(child);
-    }
-
-    public int CalculateSize()
-    {
-        return Size + Children.Sum(x => x.CalculateSize());
-    }
-}
-
 public static class Part1
 {
     public static void Run()
--- 2022/Day7/Part2.cs
+++ 2022/Day7/Part2.cs
@@ -1,7 +1,69 @@
 namespace AdventOfCode;
+
 public static class Part2
 {
     public static void Run()
     {
+        var lines = File.ReadAllLines(Path.Combine(Directory.GetCurrentDirectory(), "data.txt"));
+
+        Node root = new("/");
+        Node currentDirectory = root;
+
+        foreach (var line in lines)
+        {
+            if (line.StartsWith("$"))
+            {
+                if (line.StartsWith("$ cd"))
+                {
+                    var directory = line.Split(" ")[2];
+                    if (directory == "/")
+                    {
+                        currentDirectory = root;
+                    }
+                    else if (directory == "..")
+                    {
+                        currentDirectory = currentDirectory.Parent!;
+                    }
+                    else
+                    {
+                        currentDirectory = currentDirectory.Children.First(x => x.Name == directory);
+                    }
+                }
+            }
+            else if (line.StartsWith("dir "))
+            {
+                var directory = line.Split(" ")[1];
+                Node newDirectory = new(directory, currentDirectory);
+                currentDirectory.AddChild(newDirectory);
+            }
+            else
+            {
+                currentDirectory.Size += int.Parse(line.Split(" ")[0]);
+            }
+        }
+
+        int totalDiskSpace = 70000000;
+        int usedSpace = root.CalculateSize();
+        int freeSpace = totalDiskSpace - usedSpace;
+        int targetFreeSpace = 30000000;
+        int spaceToFree = targetFreeSpace - freeSpace;
+
+        var directoryToFree = FindSmallestDirectory(root, spaceToFree);
+        Console.WriteLine(directoryToFree);
+    }
+
+    public static int FindSmallestDirectory(Node node, int spaceToFree, int smallestSize = -1)
+    {
+        int smallestDirectorySize = smallestSize;
+        foreach (var child in node.Children)
+        {
+            var size = child.CalculateSize();
+            if (size >= spaceToFree && (smallestDirectorySize == -1 || size < smallestDirectorySize))
+            {
+                smallestDirectorySize = size;
+            }
+            smallestDirectorySize = FindSmallestDirectory(child, spaceToFree, smallestDirectorySize);
+        }
+        return smallestDirectorySize;
     }
 }
