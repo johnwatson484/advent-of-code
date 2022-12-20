5c4de91f7eb71e8fe3097c0ba9cbd3af0fab4a2f
John Watson <johnwatson484@hotmail.com>
2022-12-20 19:52:09 +0000

    Add part 2

--- 2022/Day8/Part2.cs
+++ 2022/Day8/Part2.cs
@@ -5,29 +5,82 @@ public static class Part2
     {
         var rows = File.ReadAllLines(Path.Combine(Directory.GetCurrentDirectory(), "data.txt")).Select(x => x.Select(y => int.Parse(y.ToString())).ToArray()).ToArray();
 
-        int visibleTrees = 0;
+        List<int> visibleTrees = new();
 
         for (int r = 0; r < rows.Length; r++)
         {
             for (int c = 0; c < rows[r].Length; c++)
             {
-                bool visibleUp = true;
-                bool visibleDown = true;
-                bool visibleLeft = true;
-                bool visibleRight = true;
-
-                if (r > 0) visibleUp = IsVisible(rows[r][c], rows[r - 1][c]);
-                if (r < rows.Length - 1) visibleDown = IsVisible(rows[r][c], rows[r + 1][c]);
-                if (c > 0) visibleLeft = IsVisible(rows[r][c], rows[r][c - 1]);
-                if (c < rows[r].Length - 1) visibleRight = IsVisible(rows[r][c], rows[r][c + 1]);
-                if (visibleUp || visibleDown || visibleLeft || visibleRight)
-                {
-                    visibleTrees++;
-                }
+                var scenicScore = TreesVisibleUp(rows, r, c) * TreesVisibleDown(rows, r, c) * TreesVisibleLeft(rows, r, c) * TreesVisibleRight(rows, r, c);
+                visibleTrees.Add(scenicScore);
             }
         }
 
-        Console.WriteLine(visibleTrees);
+        Console.WriteLine(visibleTrees.Max());
+    }
+
+    public static int TreesVisibleUp(int[][] rows, int row, int col)
+    {
+        int trees = 0;
+        int next = 1;
+        while (row - next + 1 > 0)
+        {
+            if (!IsVisible(rows[row][col], rows[row - next][col]))
+            {
+                return trees + 1;
+            }
+            trees++;
+            next++;
+        }
+        return trees;
+    }
+
+    public static int TreesVisibleDown(int[][] rows, int row, int col)
+    {
+        int trees = 0;
+        int next = 1;
+        while (row + next - 1 < rows.Length - 1)
+        {
+            if (!IsVisible(rows[row][col], rows[row + next][col]))
+            {
+                return trees + 1;
+            }
+            trees++;
+            next++;
+        }
+        return trees;
+    }
+
+    public static int TreesVisibleLeft(int[][] rows, int row, int col)
+    {
+        int trees = 0;
+        int next = 1;
+        while (col - next + 1 > 0)
+        {
+            if (!IsVisible(rows[row][col], rows[row][col - next]))
+            {
+                return trees + 1;
+            }
+            trees++;
+            next++;
+        }
+        return trees;
+    }
+
+    public static int TreesVisibleRight(int[][] rows, int row, int col)
+    {
+        int trees = 0;
+        int next = 1;
+        while (col + next < rows[row].Length)
+        {
+            if (!IsVisible(rows[row][col], rows[row][col + next]))
+            {
+                return trees + 1;
+            }
+            trees++;
+            next++;
+        }
+        return trees;
     }
 
     public static bool IsVisible(int current, int compare)
